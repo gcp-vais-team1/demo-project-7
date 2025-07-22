012e267ebb8c34b610dabd5f4dfc9b94b9c887da
avinashjay1 <Avinash.Veershetty@bristlecone.com>
2025-07-22 18:14:22 +0530

    Update content of config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-config = {'db': 'https://www.white-fuller.net/searchmain.asp', 'api_key': '68bd46cfe1fa14bc98c1a3167f11480eeb27902c'}
+CONFIG = {'project_id': 7, 'debug': True, 'owner': 'vais-team1'}
